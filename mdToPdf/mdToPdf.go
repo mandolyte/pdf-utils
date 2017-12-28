21af48e983ccb93929b051fee9964c5193dc5243
mandolyte <cecil.new@gmail.com>
2017-12-28 10:25:32 -0500

    added headings and new styler type

--- mdToPdf/mdToPdf.go
+++ mdToPdf/mdToPdf.go
@@ -7,6 +7,7 @@ import (
 	"io/ioutil"
 	"log"
 	"os"
+	"strings"
 
 	"github.com/jung-kurt/gofpdf"
 	bf "gopkg.in/russross/blackfriday.v2"
@@ -46,6 +47,13 @@ func main() {
 	//sContent = strings.Replace(sContent, "\t", strings.Repeat(" ", *tabwidth), -1)
 
 	pf := NewPdfRenderer()
+	pf.H1.Spacing = 24
+	pf.H2.Spacing = 22
+	pf.H3.Spacing = 20
+	pf.H4.Spacing = 5
+	pf.H5.Spacing = 3
+	pf.H6.Spacing = 0
+
 	_ = bf.Run(content, bf.WithRenderer(pf))
 
 	err = pf.pdf.OutputFileAndClose(*output)
@@ -57,40 +65,70 @@ func main() {
 
 }
 
+// Styler is the struct to capture the styling features for text
+type Styler struct {
+	Font    string
+	Style   string
+	Size    float64
+	Spacing float64
+}
+
 // PdfRenderer is the struct to manage conversion of a markdown object
 // to PDF format.
 type PdfRenderer struct {
-	pdf                                    *gofpdf.Fpdf
-	orientation, units, papersize, fontdir string
-	font, style                            string
-	fontsize, lineheight                   float64
-	currentsize                            float64
+	pdf                *gofpdf.Fpdf
+	Orientation, units string
+	Papersize, fontdir string
+
+	// current settings
+	current Styler
+
+	// normal text
+	Normal Styler
+
 	// headings
-	H1Font       string
-	H1Style      string
-	H1Size       float64
-	H1Lineheight float64
+	H1 Styler
+	H2 Styler
+	H3 Styler
+	H4 Styler
+	H5 Styler
+	H6 Styler
 }
 
 // NewPdfRenderer creates and configures an PdfRenderer object,
 // which satisfies the Renderer interface.
 func NewPdfRenderer() *PdfRenderer {
 	pdfr := new(PdfRenderer)
-	pdfr.orientation = "portrait"
+
+	// Global things
+	pdfr.Orientation = "portrait"
 	pdfr.units = "pt"
-	pdfr.papersize = "A4"
+	pdfr.Papersize = "A4"
 	pdfr.fontdir = "."
-	pdfr.font = "Arial"
-	pdfr.fontsize = 12
-	pdfr.lineheight = pdfr.fontsize + 2
-	pdfr.style = ""
-	pdfr.H1Font = "times"
-	pdfr.H1Size = 20
-	pdfr.H1Lineheight = pdfr.H1Size + 2
-	pdfr.H1Style = "b"
-	pdfr.pdf = gofpdf.New(pdfr.orientation, pdfr.units, pdfr.papersize, pdfr.fontdir)
+
+	// Normal Text
+	pdfr.Normal = Styler{Font: "Arial", Style: "", Size: 12, Spacing: 3}
+
+	// Headings
+	pdfr.H1 = Styler{Font: "Arial", Style: "b", Size: 24, Spacing: 5}
+	pdfr.H2 = Styler{Font: "Arial", Style: "b", Size: 22, Spacing: 5}
+	pdfr.H3 = Styler{Font: "Arial", Style: "b", Size: 20, Spacing: 5}
+	pdfr.H4 = Styler{Font: "Arial", Style: "b", Size: 18, Spacing: 5}
+	pdfr.H5 = Styler{Font: "Arial", Style: "b", Size: 16, Spacing: 5}
+	pdfr.H6 = Styler{Font: "Arial", Style: "b", Size: 14, Spacing: 5}
+
+	pdfr.pdf = gofpdf.New(pdfr.Orientation, pdfr.units,
+		pdfr.Papersize, pdfr.fontdir)
 	pdfr.pdf.AddPage()
 	return pdfr
+}
+
+func (r *PdfRenderer) setFont(s Styler) {
+	r.pdf.SetFont(s.Font, s.Style, s.Size)
+}
+
+func (r *PdfRenderer) write(s Styler, t string) {
+	r.pdf.Write(s.Size+s.Spacing, t)
 }
 
 // RenderNode is a default renderer of a single node of a syntax tree. For
@@ -107,47 +145,48 @@ func NewPdfRenderer() *PdfRenderer {
 func (r *PdfRenderer) RenderNode(w io.Writer, node *bf.Node, entering bool) bf.WalkStatus {
 	switch node.Type {
 	case bf.Text:
-		r.pdf.Write(r.currentsize+2, string(node.Literal))
+		r.setFont(r.current)
+		r.write(r.current, string(node.Literal))
 	case bf.Softbreak:
-		panic("Not handled: bf.Softbreak")
+		dbg("Not handled: bf.Softbreak")
 	case bf.Hardbreak:
-		panic("Not handled: bf.Hardbreak")
+		dbg("Not handled: bf.Hardbreak")
 	case bf.Emph:
 		if entering {
-			r.pdf.SetFont(r.font, "i", r.fontsize)
+			r.current.Style += "i"
 		} else {
-			r.pdf.SetFont(r.font, r.style, r.fontsize)
+			r.current.Style = strings.Replace(r.current.Style, "i", "", -1)
 		}
 	case bf.Strong:
 		if entering {
-			panic("Not handled: bf.Strong (entering)")
+			r.current.Style += "b"
 		} else {
-			panic("Not handled: bf.Strong (!entering)")
+			r.current.Style = strings.Replace(r.current.Style, "b", "", -1)
 		}
 	case bf.Del:
 		if entering {
-			panic("Not handled: bf.Del (entering)")
+			dbg("Not handled: bf.Del (entering)")
 		} else {
-			panic("Not handled: bf.Del (!entering)")
+			dbg("Not handled: bf.Del (!entering)")
 		}
 	case bf.HTMLSpan:
-		panic("Not handled: bf.HTMLSpan")
+		dbg("Not handled: bf.HTMLSpan")
 	case bf.Link:
 		// mark it but don't link it if it is not a safe link: no smartypants
 		//dest := node.LinkData.Destination
 		if entering {
-			panic("Not handled: bf.Link (entering)")
+			dbg("Not handled: bf.Link (entering)")
 		} else {
-			panic("Not handled: bf.Link (!entering)")
+			dbg("Not handled: bf.Link (!entering)")
 		}
 	case bf.Image:
 		if entering {
-			panic("Not handled: bf.Image (entering)")
+			dbg("Not handled: bf.Image (entering)")
 		} else {
-			panic("Not handled: bf.Image (!entering)")
+			dbg("Not handled: bf.Image (!entering)")
 		}
 	case bf.Code:
-		panic("Not handled: bf.Code")
+		dbg("Not handled: bf.Code")
 		//r.out(w, codeTag)
 		//escapeHTML(w, node.Literal)
 		//r.out(w, codeCloseTag)
@@ -155,36 +194,45 @@ func (r *PdfRenderer) RenderNode(w io.Writer, node *bf.Node, entering bool) bf.W
 		break
 	case bf.Paragraph:
 		if entering {
-			r.pdf.SetFont(r.font, r.style, r.fontsize)
-			r.currentsize = r.fontsize
+			r.setFont(r.Normal)
 		} else {
 			r.cr()
-			r.pdf.SetFont(r.font, r.style, r.fontsize)
-			r.currentsize = r.fontsize
 		}
 	case bf.BlockQuote:
 		if entering {
-			panic("Not handled: bf.BlockQuote (entering)")
+			dbg("Not handled: bf.BlockQuote (entering)")
 		} else {
-			panic("Not handled: bf.BlockQuote (!entering)")
+			dbg("Not handled: bf.BlockQuote (!entering)")
 		}
 	case bf.HTMLBlock:
-		panic("Not handled: bf.HTMLBlock")
+		dbg("Not handled: bf.HTMLBlock")
 	case bf.Heading:
 		//openTag, closeTag := headingTagsFromLevel(node.Level)
+		//dbg(fmt.Sprintf("Heading level is %v", node.HeadingData.Level))
 		if entering {
-			r.pdf.SetFont(r.H1Font, r.H1Style, r.H1Size)
-			r.currentsize = r.H1Size
+			switch node.HeadingData.Level {
+			case 1:
+				r.current = r.H1
+			case 2:
+				r.current = r.H2
+			case 3:
+				r.current = r.H3
+			case 4:
+				r.current = r.H4
+			case 5:
+				r.current = r.H5
+			case 6:
+				r.current = r.H6
+			}
 		} else {
 			r.cr()
-			r.pdf.SetFont(r.font, r.style, r.fontsize)
-			r.currentsize = r.fontsize
+			r.current = r.Normal
 		}
 	case bf.HorizontalRule:
 		//r.cr(w)
 		//r.outHRTag(w)
 		//r.cr(w)
-		panic("Not handled: bf.HorizontalRule")
+		dbg("Not handled: bf.HorizontalRule")
 	case bf.List:
 		/*
 			openTag := ulTag
@@ -199,9 +247,9 @@ func (r *PdfRenderer) RenderNode(w io.Writer, node *bf.Node, entering bool) bf.W
 			}
 		*/
 		if entering {
-			panic("Not handled: bf.List (entering)")
+			dbg("Not handled: bf.List (entering)")
 		} else {
-			panic("Not handled: bf.List (!entering)")
+			dbg("Not handled: bf.List (!entering)")
 		}
 	case bf.Item:
 		/*
@@ -217,17 +265,17 @@ func (r *PdfRenderer) RenderNode(w io.Writer, node *bf.Node, entering bool) bf.W
 			}
 		*/
 		if entering {
-			panic("Not handled: bf.Item (entering)")
+			dbg("Not handled: bf.Item (entering)")
 		} else {
-			panic("Not handled: bf.Item (!entering)")
+			dbg("Not handled: bf.Item (!entering)")
 		}
 	case bf.CodeBlock:
-		panic("Not handled: bf.CodeBlock")
+		dbg("Not handled: bf.CodeBlock")
 	case bf.Table:
 		if entering {
-			panic("Not handled: bf.Table (entering)")
+			dbg("Not handled: bf.Table (entering)")
 		} else {
-			panic("Not handled: bf.Table (!entering)")
+			dbg("Not handled: bf.Table (!entering)")
 		}
 	case bf.TableCell:
 		/*
@@ -239,27 +287,27 @@ func (r *PdfRenderer) RenderNode(w io.Writer, node *bf.Node, entering bool) bf.W
 			}
 		*/
 		if entering {
-			panic("Not handled: bf.TableCell (entering)")
+			dbg("Not handled: bf.TableCell (entering)")
 		} else {
-			panic("Not handled: bf.TableCell (!entering)")
+			dbg("Not handled: bf.TableCell (!entering)")
 		}
 	case bf.TableHead:
 		if entering {
-			panic("Not handled: bf.TableHead (entering)")
+			dbg("Not handled: bf.TableHead (entering)")
 		} else {
-			panic("Not handled: bf.TableHead (!entering)")
+			dbg("Not handled: bf.TableHead (!entering)")
 		}
 	case bf.TableBody:
 		if entering {
-			panic("Not handled: bf.TableBody (entering)")
+			dbg("Not handled: bf.TableBody (entering)")
 		} else {
-			panic("Not handled: bf.TableBody (!entering)")
+			dbg("Not handled: bf.TableBody (!entering)")
 		}
 	case bf.TableRow:
 		if entering {
-			panic("Not handled: bf.TableRow (entering)")
+			dbg("Not handled: bf.TableRow (entering)")
 		} else {
-			panic("Not handled: bf.TableRow (!entering)")
+			dbg("Not handled: bf.TableRow (!entering)")
 		}
 	default:
 		panic("Unknown node type " + node.Type.String())
@@ -269,26 +317,22 @@ func (r *PdfRenderer) RenderNode(w io.Writer, node *bf.Node, entering bool) bf.W
 
 // RenderHeader writes HTML document preamble and TOC if requested.
 func (r *PdfRenderer) RenderHeader(w io.Writer, ast *bf.Node) {
-	dumpNode(ast)
-	//panic("Not handled: RenderHeader")
+	dbg("Not handled: RenderHeader")
 }
 
 // RenderFooter writes HTML document footer.
 func (r *PdfRenderer) RenderFooter(w io.Writer, ast *bf.Node) {
-	dumpNode(ast)
-	//panic("Not handled: RenderFooter")
+	dbg("Not handled: RenderFooter")
 }
 
 func (r *PdfRenderer) cr() {
-	r.pdf.Write(r.currentsize+2, "\n")
+	r.write(r.current, "\n")
 }
 
 // Helper functions
-func dumpNode(n *bf.Node) {
-	fmt.Printf("Node Dump:\n%v\n", n)
+func dbg(msg string) {
+	fmt.Println(msg)
 }
-
-
 
 func usage(msg string) {
 	fmt.Println(msg + "\n")
--- mdToPdf/test.md
+++ mdToPdf/test.md
@@ -1,2 +1,17 @@
 # Heading One
 This is a paragraph with some *emphasized text* - OK?
+
+## Heading Two
+This is a paragraph with some **emphasized text** - OK?
+
+### Heading Three
+This is a paragraph with some ___emphasized text___ - OK?
+
+#### Heading Four
+*A wee bit of text*
+
+#### Heading Five
+Just some normal text... nothing fancy
+
+###### Heading Six
+This is ***REALLY*** deeply nested.
